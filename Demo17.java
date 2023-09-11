ea6ae4d7634a2a03746af50876eff9ffd79cdb50
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-11 15:18:55 +0530

    Second commit

--- Demo17.java
+++ Demo17.java
@@ -3,3 +3,14 @@ public class Demo17 {
         
     }
 }
+class Parent2{
+    int x =10;
+    void print(){
+        System.out.println(x);
+        System.out.println(this.x);
+        
+    }
+}
+ class Sub2 extends Parent2{
+    int x = 20;
+ }
