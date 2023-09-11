9eef43aed780b79af231b66e810f1af6a34cba7a
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-11 16:34:46 +0530

    second

--- Demo19.java
+++ Demo19.java
@@ -1,5 +1,23 @@
 public class Demo19 {
     public static void main(String[] args) {
+        new myChildClass();
+        new myChildClass(10);
+        new myChildClass(10, "IJSE");
         
+    }
+}
+class parentClass{
+
+}
+
+class myChildClass extends parentClass{
+    public myChildClass(){
+        super();
+    }
+    public myChildClass(int id){
+        super();
+    }
+    public myChildClass(int id,String name){
+        super();
     }
 }
