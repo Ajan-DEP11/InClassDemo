9108dcad8032ebf51a846a633664155335db9e0f
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-12 11:03:46 +0530

    Second

--- Demo22.java
+++ Demo22.java
@@ -1,5 +1,14 @@
+import java.time.LocalDateTime;
+
 public class Demo22 {
     public static void main(String[] args) {
+        User adminUser1 = new AdminUser("Prasad", "12345");
+        User adminUser2 = new AdminUser("Kasun", "1234768");
+        User guestUser3 = new GuestUsser("Nadun", "16459");
+
+          adminUser1.Login();
+          adminUser2.Login();
+          guestUser3.Login();
         
     }
 }
@@ -7,16 +16,16 @@ public class Demo22 {
     String username;
     String password;
     String role;
-    boolean isLogged;
+    boolean Logged;
 
     public void Login(){
         System.out.printf("%s logged in \n",username);
-        isLogged = true;
+        Logged = true;
 
     }
 
     public void Logout(){
-        if(isLogged){
+        if(Logged){
             System.out.printf("%s logged out \n",username);
         }else{
             System.out.println("user hs not logged in ");
@@ -30,19 +39,45 @@ public class Demo22 {
  }
 
  class AdminUser extends User{
+
+    private String[] passwords = {"IJSE_12345","IJSE_1234768"};
+    
     public AdminUser(String username,String password){
         this.username= username;
         this.password = password;
         this.role = "ROLE_ADMIN";
     }
+    public void Login(){
+       for(String password : passwords){
+        if(password.equals(this.password)){
+            System.out.printf("%s logged in at: %s \n",username,LocalDateTime.now());
+            return;
+        }
+       }
+    }
 
  }
 
  class GuestUsser extends User{
+    private static final String  GUEST_PASSWORD = "GUEST_16459";
+
 public GuestUsser(String username,String password){
         this.username= username;
         this.password = password;
         this.role = "ROLE_GUEST";
+    }
+    @Override
+
+    public void Login(){
+      
+        if(password.equals(GUEST_PASSWORD)){
+            System.out.printf("%s logged in at: %s \n",username,LocalDateTime.now());
+            return;
+        }else {
+            System.out.println("Inavalid Login");
+        }
+       
+
     }
 
 
