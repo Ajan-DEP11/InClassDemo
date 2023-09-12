8e370cf0678c9e98fbf70edf0fd34025c88eb6c1
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-12 13:26:46 +0530

    next last

--- Demo23.java
+++ Demo23.java
@@ -1,19 +1,26 @@
 public class Demo23 {
     public static void main(String[] args) {
        // Months jan = Months.MAY;
-        MonthsClone jan2 = MonthsClone.JAN;
+       // MonthsClone jan2 = MonthsClone.JAN;
+       Months.JAN.print();
         
     }
 }
 
 enum Months{
-    JAN,FEB,MAR,APR,MAY,JUNE;
+    JAN(1,"Ajan"),
+    FEB(2,"Kasun"),
+    MAR(3,"Nadun");
 
-    int id;
-    String name;
+   private int id;
+    private String name;
 
-    Months(){
-        System.out.println("Months constructor");
+    Months(int id, String name){
+       this.id = id;
+       this.name = name;
+    }
+    public void print(){
+        System.out.printf("Month:%s - %s\n",id,name);
     }
 
     // void myMethod(){
@@ -26,29 +33,29 @@ enum Months{
 
 }
 
-class MonthsClone{
-    public static final MonthsClone JAN = new MonthsClone();
-    public static final MonthsClone FEB = new MonthsClone();
-    public static final MonthsClone MAR = new MonthsClone();
-    public static final MonthsClone APR = new MonthsClone();
-    public static final MonthsClone MAY = new MonthsClone();
-    public static final MonthsClone JUNE = new MonthsClone();
+// class MonthsClone{
+//     public static final MonthsClone JAN = new MonthsClone();
+//     public static final MonthsClone FEB = new MonthsClone();
+//     public static final MonthsClone MAR = new MonthsClone();
+//     public static final MonthsClone APR = new MonthsClone();
+//     public static final MonthsClone MAY = new MonthsClone();
+//     public static final MonthsClone JUNE = new MonthsClone();
 
-    private String fieldname;
-    private static int count;
+//     private String fieldname;
+//     private static int count;
 
-    {
-        fieldname = MonthsClone.class.getDeclaredFields()[count++].getName();
-    }
+//     {
+//         fieldname = MonthsClone.class.getDeclaredFields()[count++].getName();
+//     }
 
-    // private MonthsClone(){
-    //     System.out.println("MonthsClone");
-    // }
+//     // private MonthsClone(){
+//     //     System.out.println("MonthsClone");
+//     // }
 
-    @Override
-    public String toString(){
-       return fieldname;
-    }
+//     @Override
+//     public String toString(){
+//        return fieldname;
+//     }
     
     
-}
+// }
