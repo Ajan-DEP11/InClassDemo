c23bfab3d589f0ba03105e3c685ea6f68b12c9d5
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-12 12:21:45 +0530

    second

--- Demo23.java
+++ Demo23.java
@@ -1,10 +1,42 @@
 public class Demo23 {
     public static void main(String[] args) {
+       // Months jan = Months.MAY;
+        MonthsClone jan2 = MonthsClone.JAN;
         
     }
 }
 
 enum Months{
-    JAN,FEB,MAR,APR,MAY;
+    JAN,FEB,MAR,APR,MAY,JUNE;
 
+    int id;
+    String name;
+
+    Months(){
+        System.out.println("Months constructor");
+    }
+
+    // void myMethod(){
+
+    // }
+
+    // static void myMethod2(){
+
+    // }
+
+}
+
+class MonthsClone{
+    public static final MonthsClone JAN = new MonthsClone();
+    public static final MonthsClone FEB = new MonthsClone();
+    public static final MonthsClone MAR = new MonthsClone();
+    public static final MonthsClone APR = new MonthsClone();
+    public static final MonthsClone MAY = new MonthsClone();
+    public static final MonthsClone JUNE = new MonthsClone();
+
+    private MonthsClone(){
+        System.out.println("MonthsClone");
+    }
+    
+    
 }
