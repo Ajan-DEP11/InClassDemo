ce223acf004b3fdb9bcd85d42118ef219d0746ed
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-13 13:52:07 +0530

    last

--- Hiding/Demo.java
+++ Hiding/Demo.java
@@ -12,24 +12,33 @@ class SuperClass{
     public static void execute(){
         System.out.println("Super class");
     }
+    static{
+        System.out.println("super class is being initia:");
+    }
 
 }
 
  class Dream extends SuperClass implements Idea1,Idea2{
+    static{
+        System.out.println("Dream class initializing");
+    }
 
  }
+
 
 interface SuperIdea{
    public static void execute(){
         System.out.println("super idea");
     }
 }
+
 interface Idea1 extends SuperIdea{
     public static void execute(){
         System.out.println("idea1 is an extention of the super idea");
     }
 
 }
+
 interface Idea2 {
    public static void execute(){
         System.out.println("idea2");
