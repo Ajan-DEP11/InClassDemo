85979694afa63f132d3bd723e996e25a3075f4a6
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-13 10:54:48 +0530

    second

--- Demo30.java
+++ Demo30.java
@@ -3,3 +3,31 @@ public class Demo30 {
         
     }
 }
+
+abstract class MyabstractCalss{
+    protected abstract void myMethod();
+    abstract void myMethod2(); //----> package private are also allowed
+}
+
+interface IMyInterface{ //IMyinterface convention of industry starting with "I"
+
+    //public abstract void myMethod();
+    void myMethod();
+    //abstract void myMethod --> public
+   // protected void myMethod1(); --> not allow to make protected
+
+}
+
+interface IMyInterface2 extends IMyInterface/*ImyInterface4,ImyInterface5*/{
+
+    void myMethod5();
+
+}
+
+//class MyConcreatClass implements IMyInterface2{
+   // public void myMethod5();
+
+  //  public void myMethod();
+
+
+}
